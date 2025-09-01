3c1ed6f6848d378319c747273f8b6dedde2d149f
atul <atulsrivastav768@gmail.com>
2025-09-01 14:13:07 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  const fetchCountries = async () => {
-    try {
-      const response = await fetch(
-        "https://xcountries-backend.azurewebsites.net/all",
-        { cache: "no-store" }
-      );
-      const data = await response.json();
-      setCountries(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  return (
-    <div className="App">
-      <div style={{ display: "none" }}>init</div>
-
-      {countries.map((country) => (
-        <Card key={country.name} name={country.name} flag={country.flag} />
-      ))}
-    </div>
-  );
+  return <Card />;
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const Card = ({ name, flag }) => {
+const Card = () => {
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch(
+        "https://xcountries-backend.azurewebsites.net/all",
+        { cache: "no-store" }
+      );
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
   return (
-    <div className="card">
-      <img src={flag} alt={`${name} flag`} />
-      <h2>{name}</h2>
+    <div className="App">
+      {countries.map((country) => (
+        <div key={country.name} className="card">
+          <img src={country.flag} alt={`${country.name} flag`} />
+          <h2>{country.name}</h2>
+        </div>
+      ))}
     </div>
   );
 };
